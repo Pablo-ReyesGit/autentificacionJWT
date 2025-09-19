102f3d82176ed0dea3d7c0c8b2fb2e52160b7a57
Pablo Reyes <preyesl3@miumg.edu.gt>
2025-09-18 23:21:42 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const swaggerUI = require("swagger-ui-express");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve Swagger documentation
+const swaggerSpec = require("./app/config/swagger.config.js");
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+const db = require("./app/models/index.js");
+db.sequelize.sync();
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Test
+ *     tags: [Servicios Up]
+ *     description: Test Proyecto Levantado
+ *     responses:
+ *       200:
+ *         description: Test Proyecto Levantado
+ */
+app.get("/", (req, res) => {
+  res.json({ message: "UMG Web Universidad" });
+});
+try{
+  require("./app/routes/usuario.routes.js")(app);
+  console.log("✅ usuario.routes.js cargado correctamente");
+}catch(err){
+  console.error("❌ Error al cargar usuario.routes.js:", err.message);
+}
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Servidor levantado en puerto ${PORT}.`);
+});
